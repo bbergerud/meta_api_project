bdfb46194efbbf9cac9264a53c5d8652b7f9fc23
bbergerud <brandon.bergerud@gmail.com>
2024-05-23 18:03:16 -0400

    added additional validators

--- LittleLemonAPI/models.py
+++ LittleLemonAPI/models.py
@@ -23,7 +23,12 @@ class Category(models.Model):
 
 class MenuItem(models.Model):
     title = models.CharField(max_length=255, db_index=True)
-    price = models.DecimalField(max_digits=6, decimal_places=2, db_index=True)
+    price = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        db_index=True,
+        validators=[MinValueValidator(decimal.Decimal("0.00"))],
+    )
     featured = models.BooleanField(db_index=True)
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
 
@@ -34,9 +39,17 @@ class MenuItem(models.Model):
 class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     menuitem = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
-    quantity = models.SmallIntegerField()
-    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    quantity = models.SmallIntegerField(validators=[MinValueValidator(1)])
+    unit_price = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        validators=[MinValueValidator(decimal.Decimal("0.00"))],
+    )
+    price = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        validators=[MinValueValidator(decimal.Decimal("0.00"))],
+    )
 
     class Meta:
         unique_together = ("menuitem", "user")
