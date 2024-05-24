26261ccc20060297284b5482586bfc6f5004e5d2
bbergerud <brandon.bergerud@gmail.com>
2024-05-23 23:21:38 -0400

    removed trailing slash

--- LittleLemon/urls.py
+++ LittleLemon/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter(trailing_slash=False)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
